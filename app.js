bce1395d1d220f810d31d0b93c49a6d40d5253df
Steven Mendez <96746466+MinecraftSt3v3@users.noreply.github.com>
2022-04-07 15:20:46 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
  * And the current time 
  */
 
-
+/*
 // Display name
 var disName = ' Steven Mendez :)';
 // Display name
@@ -25,3 +25,37 @@ const time = date.toLocaleTimeString();
 // display date and time 
 console.log('Date: ' + n);
 console.log('Time: ' + time);
+*/
+
+
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const port = 3000;
+// program to display the date
+
+
+
+app.get('/', (req, res) => {
+
+  // get local machine date time
+const date = new Date();
+
+// get the date as a string
+const n = date.toDateString();
+
+// get the time as a string
+const time = date.toLocaleTimeString();
+
+
+res.write(('<h1> My name is Steven Mendez </h1>'));
+res.write(('<h1> Date: ' + n + ' </h1>'));
+res.write(('<h1>Time:  ' + time + '</h1>' ));
+
+res.end();
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
